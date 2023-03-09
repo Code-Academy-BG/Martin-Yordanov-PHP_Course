c256783fab3fd2175c837e0492fd57c91f51cc5f
Martin <77927455+Marti-maker@users.noreply.github.com>
2023-03-09 14:45:02 +0200

    Delete Lecture for string manipulations directory

--- Lecture for string manipulations/ex3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$text="Martin's dream is to be developer.";
-$manipulatedText=strtoupper($text);
-
-echo "Text before capitalization:[$text] and the text after capitalization:[$manipulatedText].";
-
-
-?>
--- Lecture for string manipulations/ex6.php
+++
@@ -1,6 +0,0 @@
-<?php
-$loremIpsin="Lorem Ipsum";
-
-echo str_pad($loremIpsin, 30," ", STR_PAD_BOTH);
-
-?>
